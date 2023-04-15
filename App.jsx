842e8420825aee631a254d067a8b7c0d6ba28cfd
AliSiddique <alisiddique10@hotmail.com>
2023-04-15 12:52:13 +0100

    tab bar

--- App.jsx
+++ App.jsx
@@ -9,39 +9,33 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SFSymbol } from "react-native-sfsymbols";
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from './src/Home';
 import SettingsScreen from './src/Settings';
 
 const Tab = createBottomTabNavigator();
+const queryClient = new QueryClient();
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-
-
   return (
     <NavigationContainer>
-      <Tab.Navigator  screenOptions={{headerShown:false}}>
+                      <QueryClientProvider client={queryClient}>
+      <Tab.Navigator style={{zIndex:0}} screenOptions={{headerShown:false}}>
         <Tab.Screen name="Home"
           options={{
             tabBarLabel:() => {return null},
-            tabBarIcon: () => (
+            tabBarIcon: ({focused}) => (
               <SFSymbol
               name="house"
               weight="semibold"
               scale="large"
-              color="black"
+              color={focused ? "red": "blue"}
               size={16}
               resizeMode="center"
               multicolor={true}
@@ -104,6 +98,8 @@ function App() {
         component={SettingsScreen} />
 
       </Tab.Navigator>
+      </QueryClientProvider>
+
     </NavigationContainer>
 
 
